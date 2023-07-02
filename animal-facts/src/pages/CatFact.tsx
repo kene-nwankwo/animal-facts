85df6dd143ccd447f0177bc28c8c4dd60bcaf34e
kene-nwankwo <78567305+kene-nwankwo@users.noreply.github.com>
2023-07-02 00:26:24 -0400

    added cat fact api, with issues, double api call

--- animal-facts/src/pages/CatFact.tsx
+++ animal-facts/src/pages/CatFact.tsx
@@ -1,3 +1,8 @@
+import Post from '../Post'
 export default function CatFact(){
-    return <h1>Cat Fact</h1>
+    const myArray = ["https://meowfacts.herokuapp.com/", "text"]
+
+    return (<><h1>Cat Fact</h1>
+    <Post arrayProp={myArray} />
+    </>)
 }
